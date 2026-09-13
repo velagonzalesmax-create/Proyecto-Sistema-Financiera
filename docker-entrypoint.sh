c04420267c434e64caf96306dde2bf5cd2135463
Max <velagonzalesmax@gmail.com>
2026-09-13 18:45:52 -0500

    Fix LF line endings and EOF in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set -e
 
-# Generar clave de aplicación si no está en cache
 php artisan key:generate --force || true
-
-# Limpiar y optimizar cachés de Laravel
 php artisan config:clear
 php artisan cache:clear
-
-# Ejecutar las migraciones a Aiven automáticamente
 php artisan migrate --force || true
 
-# Iniciar Apache
 exec apache2-foreground
